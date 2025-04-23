778e9065c42c7bda0453bedf1e264b38ccf9d3a9
dcvdiego <diegochuman@gmail.com>
2025-04-23 15:21:13 +0100

    remove wrangler and exclude gif from build
    ended up changing from workers to pages and removed gif due to size limit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import { fileURLToPath } from 'node:url';
+
+const filesNeedToExclude = ['public/DeskSpacerPreAlphaDemo.gif'];
+
+const filesPathToExclude = filesNeedToExclude.map((src) => {
+  return fileURLToPath(new URL(src, import.meta.url));
+});
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -13,7 +20,12 @@ export default defineConfig({
     setupFiles: './vitest.setup.ts',
   },
   optimizeDeps: { esbuildOptions: { target: 'esnext' } },
-  build: { target: 'esnext' },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      external: [...filesPathToExclude],
+    },
+  },
 
   resolve: {
     dedupe: ['three'],
